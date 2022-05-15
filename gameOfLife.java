3f775c73c799015d6308665caee9cdf795b4dc0f
riginle <riginle@whs.school.nz>
2022-05-16 11:37:41 +1200

    laptop to PC

--- gameOfLife.java
+++ gameOfLife.java
@@ -10,96 +10,88 @@ public class gameOfLife
 {
     // instance variables - replace the example below with your own
     Random rand = new Random();
-    public int Columns = 20;
-    public int Rows = 20;
-    
-    // int[][] grid = 
-    
-                      // { { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },	
-                        // { 0, 0, 0, 1, 1, 0, 0, 0, 0, 0 },	
-                        // { 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
-			// { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-			// { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-			// { 0, 0, 0, 1, 1, 0, 0, 0, 0, 0 },
-			// { 0, 0, 1, 1, 0, 0, 0, 0, 0, 0 },
-			// { 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
-			// { 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
-			// { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
-		// };
-    
-		
-		
-		int[][] grid = 
-    
-                      { { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },	
-                        { 0, 0, 0, 1, 1, 0, 0, 0, 0, 0 },	
-                        { 0, 0, 0, 1, 1, 0, 0, 0, 0, 0 },
-			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
-		};
+    public int Columns = 10;
+    public int Rows = 10;
+
+    // int[][] board = 
+
+    // { { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },    
+    // { 0, 0, 0, 1, 1, 0, 0, 0, 0, 0 },    
+    // { 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+    // { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+    // { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+    // { 0, 0, 0, 1, 1, 0, 0, 0, 0, 0 },
+    // { 0, 0, 1, 1, 0, 0, 0, 0, 0, 0 },
+    // { 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+    // { 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+    // { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+    // };
+
+    int[][] board = 
+        { { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },    
+            { 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },    
+            { 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 1, 1, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 1, 1, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+        };
     //public boolean ShowGeneration = false;
 
     public gameOfLife() {
         System.out.println('\u000c');
-        
-        int board[][] = new int[Rows][Columns];
-        for(int x = 0; x<Columns; x++){
-            for(int y = 0; y<Rows; y++){
-                board[x][y] = (rand.nextInt(2))*(rand.nextInt(2));
-                System.out.print(board [x] [y] + "  ");
+        //int board[][] = new int[Rows][Columns];
+        for(int i = 0; i<Columns; i++){
+            for(int j = 0; j<Rows; j++){
+                //board[x][y] = (rand.nextInt(2))*(rand.nextInt(2));
+                System.out.print(board [i] [j] + "  ");
             }
             System.out.println();
         }
-        
+        System.out.println();
+        nextGeneration(board, Columns, Rows);
     }
     
-    // Function to print next generation
-	public void nextGeneration(int grid[][], int Rows, int Columns)
-	{
-		int[][] future = new int[Rows][Columns];
+    public void nextGeneration(int board[][], int Rows, int Columns)
+    {
+        int[][] future = new int[Rows][Columns];
+        for (int l = 0; l < Rows; l++)
+        {
+            for (int m = 0; m < Columns; m++)
+            {
+                
+                int aliveNeighbours = 0;
+                for (int i = -1; i <= 1; i++)
+                    for (int j = -1; j <= 1; j++)
+                        if ((l+i>=0 && l+i<Rows) && (m+j>=0 && m+j<Columns))
+                            aliveNeighbours += board[l + i][m + j]; 
+                aliveNeighbours -= board[l][m];
+                // Cell is lonely and dies
+                if ((board[l][m] == 1) && (aliveNeighbours < 2))
+                    future[l][m] = 0;
 
-		// Loop through every cell
-		for (int l = 0; l < Rows; l++)
-		{
-			for (int m = 0; m < Columns; m++)
-			{
-				// finding no Of Neighbours that are alive
-				int aliveNeighbours = 0;
-				for (int i = -1; i <= 1; i++)
-					for (int j = -1; j <= 1; j++)
-					if ((l+i>=0 && l+i<Rows) && (m+j>=0 && m+j<Columns))
-						aliveNeighbours += grid[l + i][m + j];
-
-				// The cell needs to be subtracted from
-				// its neighbours as it was counted before
-				aliveNeighbours -= grid[l][m];
-
-				// Implementing the Rules of Life
-
-				// Cell is lonely and dies
-				if ((grid[l][m] == 1) && (aliveNeighbours < 2))
-					future[l][m] = 0;
-
-				// Cell dies due to over population
-				else if ((grid[l
-				][m] == 1) && (aliveNeighbours > 3))
-					future[l][m] = 0;
-
-				// A new cell is born
-				else if ((grid[l][m] == 0) && (aliveNeighbours == 3))
-					future[l][m] = 1;
-
-				// Remains the same
-				else
-					future[l][m] = grid[l][m];
-			}
-		}
+                // Cell dies due to over population
+                else if ((board[l
+                    ][m] == 1) && (aliveNeighbours > 3))
+                    future[l][m] = 0;
+                // A new cell is born
+                else if ((board[l][m] == 0) && (aliveNeighbours == 3))
+                    future[l][m] = 1;
+                // Remains the same
+                else
+                    future[l][m] = board[l][m];
+            }
+        }
+        for(int i = 0; i<Columns; i++){
+            for(int j = 0; j<Rows; j++){
+                
+                System.out.print(future [i] [j] + "  ");
+            }
+            System.out.println();
+        }
+    }
 }
-}
-
 
