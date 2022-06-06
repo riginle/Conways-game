575c4f644ab5de07ae272dadc7aca02b100bdfd1
riginle <riginle@whs.school.nz>
2022-06-07 10:34:43 +1200

    after covid, week 6 7/06/2022

--- gameOfLife.java
+++ gameOfLife.java
@@ -13,10 +13,9 @@ public class gameOfLife
 {
     // instance variables - replace the example below with your own
     Random rand = new Random();
-    //public int Columns = 10;
-    //public int Rows = 10;
-    public String Alive = "X";
-    public String Death = " ";
+
+    String Alive = "X";
+    String Death = " ";
 
     // int[][] board =
     // { { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },    
@@ -30,208 +29,219 @@ public class gameOfLife
     // { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
     // { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
     // };
-    public boolean selectionScreen = false;
-
+    boolean selectionScreen = false;
+    boolean menuScreen = true;
+    int timerInput;
+    int[][] future;
+    int board[][];
+    int aliveInput;
+    int countDead;
+    int genInput;
+    int numberFix;
     public gameOfLife() {
         System.out.println('\u000c');
-
-        System.out.println("How many rows and columns do you want? only input one number.");
+        
+        slowPrint("How many rows and columns do you want? only input one number.");
         Scanner keyboardIn = new Scanner(System.in);
 
         int Columns = keyboardIn.nextInt();
         keyboardIn.nextLine();
         int Rows = Columns;
 
-        System.out.println("This is the board");
-        // int Rows = keyboardIn.nextInt();
-        // keyboardIn.nextLine();
+        slowPrint("This is the board");
+        System.out.println();
+        board = new int[Rows][Columns];
+        printBoard( board, Rows, Columns);
+       
+        while (menuScreen == true){
+            slowPrint("Do you want to enter selction screen? type yes if, yes. no if, no. if you want to quit the program, quit");
 
-        //int GenInput = keyboardIn.nextInt();
-        //  keyboardIn.nextLine();
+            String keyInput = keyboardIn.nextLine().toLowerCase();
 
-        int board[][] = new int[Rows][Columns];
-        for(int yModifer= 0; yModifer<Columns; yModifer++){
-            for(int xModifer = 0; xModifer<Rows; xModifer++){
-                board[yModifer][xModifer] = (rand.nextInt(2))*(rand.nextInt(2));
-                System.out.print(board [yModifer] [xModifer] + "  ");
+            if (keyInput.equalsIgnoreCase("yes")){
+                selectionScreen = true;
+            }
+
+            else if(keyInput.equalsIgnoreCase("no")){
+                nextGeneration(board, Columns, Rows);
+            }
+            
+            else if (keyInput.equalsIgnoreCase("quit")){
+                menuScreen = false;
+                return;
+            }
+
+            else {
+                slowPrint("Invalid command");
             }
             System.out.println();
-        }
-        System.out.println();
 
-        // System.out.println("do you want to enter selction screen?");
+            while (selectionScreen == true){
 
-        // String keyInput = keyboardIn.nextLine().toLowerCase();
+                slowPrint("Please select row: ");
+                int rowSelection = keyboardIn.nextInt();
+                slowPrint("Please select column: ");
+                int columnSelection = keyboardIn.nextInt();
 
-        // if (keyInput.equalsIgnoreCase("start select")){
-            // selectionScreen = true;
-            // System.out.println("do you wantrtgrrzrhrhdzrhzhrhz to enter selction screen?");
-        // }
+                if (board[rowSelection][columnSelection] == 0){
+                    board[rowSelection][columnSelection] = 1;
+                }
+                else
+                {
+                    board[rowSelection][columnSelection] = 0;
 
-        // while (selectionScreen == true){
+                }
+                for(int yModifer= 0; yModifer<Columns; yModifer++){
+                    for(int xModifer = 0; xModifer<Rows; xModifer++){
+                        System.out.print(board [yModifer] [xModifer] + "  ");
+                    }
+                    System.out.println();
+                }
+                System.out.println();
 
-            // System.out.println("Please select row: ");
-            // int rowSelection = keyboardIn.nextInt();
-            // System.out.println("Please select column: ");
-            // int columnSelection = keyboardIn.nextInt();
+                selectionScreen=false;
+            }
 
-           
-            
-            // if (board[rowSelection][columnSelection] == 0){
-                // board[rowSelection][columnSelection] = 1;
-            // }
-            // else
-            // {
-                // board[rowSelection][columnSelection] = 0;
 
-            // }
-            // for(int yModifer= 0; yModifer<Columns; yModifer++){
-                // for(int xModifer = 0; xModifer<Rows; xModifer++){
-                    // System.out.print(board [yModifer] [xModifer] + "  ");
-                // }
-                // System.out.println();
-            // }
-            // System.out.println();
-            
-            // selectionScreen=false; 
-        // }
-        
-        // System.out.println("do you want to continuw selction screen?");
-        
-        // keyboardIn.nextLine().toLowerCase();
-        
-        // if (keyInput.equalsIgnoreCase("start select")){
-            // selectionScreen = true;
-            // System.out.println("do you wantrtgrrzrhrhdzrhzhrhz to enter selction screen?");
-        // }
-        
+        }            
         nextGeneration(board, Columns, Rows);
     }
 
+   
+   
+
+   
+   
+   
     public void nextGeneration(int board[][], int Rows, int Columns)
     {
 
-        int countDead = 0;
+        countDead = 0;
 
         Scanner keyboardIn = new Scanner(System.in);
 
-        System.out.println("How many Generations would you like?");
-        int genInput = keyboardIn.nextInt();
+        slowPrint("How many Generations would you like?");
+
+        genInput = keyboardIn.nextInt();
+
         keyboardIn.nextLine();
 
-        int numberFix = 20;
+        numberFix = 20;
 
-        // String Width = keyboardIn.nextLine().replaceAll("\\D", "");
-        // if(Width != "") //If statement checks Width string is null
-        // {
-        // numberFix = Integer.parseInt(Width); //Replaces all non-numbers with nothing
-        // System.out.println("WIDTH set to "+numberFix+"\n");
-        // } else
-        // {
-        // System.out.println("ass");
-        // }
+        slowPrint("How many miliseconds between generations would you like?");
 
-        System.out.println("How many miliseconds between generations would you like?");
+        timerInput = keyboardIn.nextInt();
 
-        int timerInput = keyboardIn.nextInt();
         keyboardIn.nextLine();
 
-        System.out.println("What would you like to represent your alive cells?");
+        slowPrint("What would you like to represent your alive cells?");
 
         String aliveInput = keyboardIn.nextLine();
 
         for (int Gen = 0; Gen < genInput; Gen++)
         {
-            try {
-                TimeUnit.MILLISECONDS.sleep(timerInput);
-            }
-            catch (Exception e) {
 
-            }
+            timeWaitBetweenGens();
+
             System.out.println('\u000c');
             System.out.println("There are " + Gen + " Generations");
-            int[][] future = new int[Rows][Columns];
 
-            for (int y= 0; y< Rows; y++)
-            {
-                for (int x= 0; x< Columns; x++)
-                {
+            conwaysRules(board, Rows,  Columns);
 
-                    int aliveNeighbours = 0;
-                    for (int yModifer= -1; yModifer<= 1; yModifer++)
-                        for (int xModifer = -1; xModifer <= 1; xModifer++)
-                            if ((y+yModifer>=0 && y+yModifer<Rows) && (x+xModifer>=0 && x+xModifer<Columns))
-                                aliveNeighbours += board[y+ yModifer][x+ xModifer];
-                    aliveNeighbours -= board[y][x];
-                    // Cell is lonely and dies
-                    if ((board[y][x] == 1) && (aliveNeighbours < 2))
-                        future[y][x] = 0;
-
-                    // Cell dies due to over population
-                    else if ((board[y][x] == 1) && (aliveNeighbours > 3))
-                        future[y][x] = 0;
-                    // A new cell is born
-                    else if ((board[y][x] == 0) && (aliveNeighbours == 3))
-                        future[y][x] = 1;
-                    // Remains the same
-                    else
-                        future[y][x] = board[y][x];
-                }
-            }
-
-            for(int yModifer = 0; yModifer<Columns; yModifer++){
-                for(int xModifer = 0; xModifer<Rows; xModifer++){
-
-                    if(future[yModifer][xModifer] == 1){
-                        System.out.print(""+ aliveInput +" ");
-                    }
-                    else{
-                        System.out.print("  ");
-                    }
-
-                    //System.out.print(future [yModifer] [xModifer] + "  ");
-                }
-                System.out.println();
-            }
-
-            //for (int deadModifer = 0; deadModifer < 1; deadModifer++){
-            // System.out.println("AHHggggggHHHHHHH");
-            // if (board == future) {
-            // countDead++;
-            // System.out.println("AHHHHHHHHHHHH");
-            // }
-            // // }
-            //System.out.println("" + countDead + "");
+            printAlive(board, Rows,  Columns);
 
             if (Arrays.deepEquals(board, future)){
-
-                System.out.println("Simulation over");
+                slowPrint("Simulation over");
                 return;
+                
             }
-
-            // int deadBoard = 0;
-            // for(int yModifer = 0; yModifer<Columns; yModifer++){
-            // for(int xModifer = 0; xModifer<Rows; xModifer++){
-
-            // deadBoard += future[yModifer][xModifer];
-
-            // //System.out.print(future [yModifer] [xModifer] + "  ");
-            // }
-            // if(deadBoard == 0){
-            // System.out.println("Fransoaw");
-            // return;
-
-            // }
-            // }
-
-            System.out.println();
             board = future;
-            //future = board;
+
         }
     }
 
-    public void printFuture(){
+    void timeWaitBetweenGens(){
+        try {
+            TimeUnit.MILLISECONDS.sleep(timerInput);
+        }
+        catch (Exception e) {
 
+        }
+    }
+
+    void conwaysRules(int board[][], int Rows, int Columns){
+        future = new int[Rows][Columns];
+
+        for (int y= 0; y< Rows; y++)
+        {
+            for (int x= 0; x< Columns; x++)
+            {
+
+                int aliveNeighbours = 0;
+                for (int yModifer= -1; yModifer<= 1; yModifer++)
+                    for (int xModifer = -1; xModifer <= 1; xModifer++)
+                        if ((y+yModifer>=0 && y+yModifer<Rows) && (x+xModifer>=0 && x+xModifer<Columns)){
+                        
+                        aliveNeighbours += board[y+ yModifer][x+ xModifer];
+                    }
+                aliveNeighbours -= board[y][x];
+                // Cell is lonely and dies
+                if ((board[y][x] == 1) && (aliveNeighbours < 2))
+                    future[y][x] = 0;
+
+                // Cell dies due to over population
+                else if ((board[y][x] == 1) && (aliveNeighbours > 3))
+                    future[y][x] = 0;
+                // A new cell is born
+                else if ((board[y][x] == 0) && (aliveNeighbours == 3))
+                    future[y][x] = 1;
+                // Remains the same
+                else
+                    future[y][x] = board[y][x];
+            }
+        }
+    }
+
+    void printAlive(int board[][], int Rows, int Columns){
+        for(int yModifer = 0; yModifer<Columns; yModifer++){
+            for(int xModifer = 0; xModifer<Rows; xModifer++){
+
+                if(future[yModifer][xModifer] == 1){
+                    System.out.print(""+ aliveInput +" ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void slowPrint(String output) {
+        // this is for making text print out slower
+        // it was taken from the internet i do not understand how it works (it just looks nice)        
+        for (int i = 0; i<output.length(); i++) {
+            char c = output.charAt(i);
+            System.out.print(c);
+            try {
+                TimeUnit.MILLISECONDS.sleep(5);
+            }
+            catch (Exception e) {
+            }
+        }
+    }
+   
+    void printBoard (int board[][], int Rows, int Columns){
+        //board = new int[Rows][Columns];
+        for(int yModifer= 0; yModifer<Columns; yModifer++){
+            for(int xModifer = 0; xModifer<Rows; xModifer++){
+                board[yModifer][xModifer] = (rand.nextInt(2))*(rand.nextInt(2));
+                slowPrint(board [yModifer] [xModifer] + "  ");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 }
-
